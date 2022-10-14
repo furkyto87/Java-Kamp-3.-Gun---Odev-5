d40cbc70703fef6d7bf6cc55539d3d7e9f877465
furkyto87 <35343468+furkyto87@users.noreply.github.com>
2022-10-14 20:48:39 +0300

    Add files via upload

---
+++ GameProject/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GameProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer { 
+                Id = 1,
+                BirthYear = 1984,
+                FirstName = "ENGİN",
+                LastName = "DEMİROĞ",
+                IdentityNumber = 12345
+            });
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
